24503bdf43b586bbb228f320fa4598b4f78a1f1d
qqdyta <qqdyta@163.com>
2024-05-28 14:46:49 +0800

    更新报错

--- wifi2g_tx/802.11n_ch6_start.sh
+++ wifi2g_tx/802.11n_ch6_start.sh
@@ -17,7 +17,7 @@ calibrator wlan0 cc33xx_plt set_tx -default 0
 sleep 1
 
 # 设置802.11n模式的发射参数
-calibrator wlan0 cc33xx_plt set_tx -preamble_type 4 -phy_rate 7 -pkt_mode 0 -tx_power 10 -src_addr 04:05:05:05:05:04 -dst_addr 06:07:07:07:07:06
+calibrator wlan0 cc33xx_plt set_tx -preamble_type 2 -phy_rate 7 -pkt_mode 0 -tx_power 10 -src_addr 04:05:05:05:05:04 -dst_addr 06:07:07:07:07:06
 sleep 1
 
 # 启动TX
